09d2e260655db4c920926d89f18e0b189853ae33
Kenny Larsen <98322694+Ken9Kodehode@users.noreply.github.com>
2022-02-15 10:47:37 +0100

    Add files via upload

---
+++ del1.js
@@ -0,0 +1,9 @@
+const parts = ["Paint", "Wheels", "Engine", "Mirrors", "Wheelcaps", "Doors", "Seats", "Spare wheel", "Screws", "steeringwheel"];
+console.log(parts)
+parts.splice("7");
+parts.unshift("Kenny-Person")
+console.log(parts)
+parts.push("Crow")
+console.log(parts)
+parts.splice("11")
+console.log(parts)
---
+++ del2.js
@@ -0,0 +1,9 @@
+let x = 3.141592653589793238462643383279502884197169399375105820974944592307816406286;
+let y = 2.71828;
+x = x.toFixed(5);
+y = y.toFixed(4)
+console.log(x)
+console.log(y)
+
+let result = x * y 
+console.log(result)
